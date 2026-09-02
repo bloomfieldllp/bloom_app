9749eff6605c5c999c6f680a77f527ba4d186458
Dhruvpatel78789 <76432810+Dhruvpatel78789@users.noreply.github.com>
2026-09-02 11:08:44 +0530

    Fix operator project assignment data synchronization

--- routes/sync.py
+++ routes/sync.py
@@ -240,7 +240,25 @@ async def api_pull(req: PullRequest):
     if since_dt:
         school_query["updated_at"] = {"$gt": since_dt}
         project_query["updated_at"] = {"$gt": since_dt}
-        student_query["updated_at"] = {"$gt": since_dt}
+        
+        # If a project was updated recently (e.g. operator assignment changed),
+        # we MUST pull all its students regardless of when the student was updated.
+        recently_updated_projects = list(db.projects.find({
+            "_id": {"$in": [ObjectId(pid) for pid in project_ids]},
+            "updated_at": {"$gt": since_dt}
+        }))
+        recently_updated_project_ids = [str(p["_id"]) for p in recently_updated_projects]
+        
+        if recently_updated_project_ids:
+            student_query = {
+                "project_id": {"$in": project_ids},
+                "$or": [
+                    {"updated_at": {"$gt": since_dt}},
+                    {"project_id": {"$in": recently_updated_project_ids}}
+                ]
+            }
+        else:
+            student_query["updated_at"] = {"$gt": since_dt}
         
     schools_up = list(db.schools.find(school_query))
     projects_up = list(db.projects.find(project_query))
